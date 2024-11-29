12fe930a9277f5632c144d541801264b383f19fd
chrisbishop-looka <chris@looka.com>
2024-11-29 10:25:21 -0500

    fix: add color and orientation

--- unsplash/search_service.go
+++ unsplash/search_service.go
@@ -30,9 +30,11 @@ type SearchService service
 
 // SearchOpt should be used to query /search endpoint
 type SearchOpt struct {
-	Page    int    `url:"page"`
-	PerPage int    `url:"per_page"`
-	Query   string `url:"query"`
+	Page        int    `url:"page"`
+	PerPage     int    `url:"per_page"`
+	Query       string `url:"query"`
+	Color       string `url:"color,omitempty"`
+	Orientation string `url:"orientation,omitempty"`
 }
 
 // Valid validates a SearchOpt
